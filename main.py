e156ff64cb0a58713c8c2c6a31124b21ce59151b
zoeyuchao <zoeyuchao@1625360790@qq.com>
2019-04-20 02:41:58 +0800

    gif

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from multiagent.multi_discrete import MultiDiscrete
 from knockknock import slack_sender
 
 webhook_url = "https://hooks.slack.com/services/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"
-@slack_sender(webhook_url=webhook_url, channel="train", user_mentions=["zoeyuchao"])
+#@slack_sender(webhook_url=webhook_url, channel="train", user_mentions=["zoeyuchao"])
 def train_your_nicest_model():
     run(config)
 
